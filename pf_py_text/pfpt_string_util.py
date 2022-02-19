66813d6dfaeb33912b446657fc44f03ca28c6d1a
hmtmcse <hmtm.cse@gmail.com>
2022-02-19 19:21:18 +0600

    Move String util to Text module

---
+++ pf_py_text/pfpt_string_util.py
@@ -0,0 +1,33 @@
+import re
+
+
+class PFPTStringUtil:
+
+    @staticmethod
+    def underscore_to_camelcase(word):
+        return ''.join(x.capitalize() or '_' for x in word.split('_'))
+
+    @staticmethod
+    def find_replace_into_text(content: str, key_value: dict):
+        for key in key_value:
+            value = key_value[key]
+            if not value:
+                value = ""
+            content = content.replace(key, value)
+        return content
+
+    @staticmethod
+    def camelcase_to(text: str, to: str = "_"):
+        return re.sub(r'(?<!^)(?=[A-Z])', to, text)
+
+    @staticmethod
+    def camelcase_to_lower(text: str, to: str = "_"):
+        return PFPTStringUtil.camelcase_to(text, to).lower()
+
+    @staticmethod
+    def replace_space_with(text: str, to: str = "_"):
+        return re.sub('\s+', to, text)
+
+    @staticmethod
+    def find_and_replace_with(text: str, find: any, replace: any):
+        return text.replace(find, replace)
